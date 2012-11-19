c00c88cbbb81b0ccefc011ff05e35db61e62c412
Finn Johnsen <johnsenf@gmail.com>
2012-11-19 01:55:36 +0100

    following the tutorial

--- init.el
+++ init.el
@@ -1,2 +1,15 @@
 (setq initial-scratch-message "")
 (setq make-backup-files nil)
+(put 'eval-expression 'disabled nil)
+(show-paren-mode) 
+
+;;; stuff from the giraffe book:
+(global-set-key "\C-x\ n"  'other-window)
+(global-set-key "\C-x\ p"  'other-window-backward)
+
+(defun other-window-backward (&optional n)
+  "Select the previous window."
+  (interactive "p")
+  (if n
+      (other-window (- n)))
+  (other-window -1))
