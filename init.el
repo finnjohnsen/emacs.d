1d46597b9f708e4d9f0b32072e6e3a0344acc55d
Finn Johnsen <johnsenf@gmail.com>
2012-11-28 00:33:38 +0100

    added line number, also removed the temporary files generation.

--- init.el
+++ init.el
@@ -2,6 +2,12 @@
 (setq make-backup-files nil)
 (put 'eval-expression 'disabled nil)
 (show-paren-mode) 
+(global-linum-mode t)
+
+(setq backup-directory-alist
+          `((".*" . ,temporary-file-directory)))
+    (setq auto-save-file-name-transforms
+          `((".*" ,temporary-file-directory t)))
 
 ;;; stuff from the giraffe book:
 (global-set-key "\C-x\ n"  'other-window)
