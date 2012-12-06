9d0f85bb92b69584a93141d0cdd0f322df558c2c
Finn Johnsen <johnsenf@gmail.com>
2012-12-06 23:31:02 +0100

    made a wrapper for restoring cursor. felt more lispy.

--- move-line.el
+++ move-line.el
@@ -1,35 +1,37 @@
 (provide 'move-line)
+
 (defun move-line-up ()
   "moves current line 1 up"
   (interactive)
-  (move-current-line t)
-)
+  (move-current-line t))
+
 (defun move-line-down ()
   "moves current line 1 down"
   (interactive)
-  (move-current-line)
-)
-(defun move-current-line (&optional up)
-  "moved current line up or down according to argument given"
+  (move-current-line))
+
+(defun save-column (func)
+  "restored current column after invoking func"
   (let ((column (current-column)))
-    (if up (progn
-	       (move-beginning-of-line())
-	       (kill-whole-line)
-	       (previous-line)
-	       (move-beginning-of-line())
-	       (yank)
-	       (previous-line))
-      (progn
-	(move-beginning-of-line())
-	(kill-whole-line)
-	(next-line)
-	(yank)
-	(previous-line)
-	))
-    (move-to-column column)))
+    (funcall func)
+     (move-to-column column)))
 
-
-
+(defun move-current-line (&optional up)
+  "moves current line up or down, argument decides which way"
+  (save-column(lambda ()
+		(if up (progn
+			 (move-beginning-of-line())
+			 (kill-whole-line)
+			 (previous-line)
+			 (move-beginning-of-line())
+			 (yank)
+			 (previous-line))
+		  (progn
+		    (move-beginning-of-line())
+		    (kill-whole-line)
+		    (next-line)
+		    (yank)
+		    (previous-line))))))
 
 
 
