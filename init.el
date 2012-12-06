4239bfd3e5ce117e7a727e00ee577abede922d96
Finn Johnsen <johnsenf@gmail.com>
2012-12-06 22:24:07 +0100

    added move-line to init.el

--- init.el
+++ init.el
@@ -1,21 +1,38 @@
 (setq initial-scratch-message "")
+(put 'set-goal-column 'disabled nil)
 (setq make-backup-files nil)
 (put 'eval-expression 'disabled nil)
 (show-paren-mode) 
-(global-linum-mode t)
+(setq global-linum-mode t)
+(setq column-number-mode t)
+(add-to-list 'load-path "~/.emacs.d/addons/")
+(require 'key-chord)
+(key-chord-mode 1)
+(key-chord-define-global "xe"     'eval-last-sexp)
 
-(setq backup-directory-alist
-          `((".*" . ,temporary-file-directory)))
-    (setq auto-save-file-name-transforms
-          `((".*" ,temporary-file-directory t)))
 
-;;; stuff from the giraffe book:
-(global-set-key "\C-x\ n"  'other-window)
-(global-set-key "\C-x\ p"  'other-window-backward)
+(add-to-list 'load-path "~/.emacs.d")
+(require 'move-line)
+(global-set-key [M-up] 'move-line-up)
+(global-set-key [M-down] 'move-line-down)
 
-(defun other-window-backward (&optional n)
-  "Select the previous window."
-  (interactive "p")
-  (if n
-      (other-window (- n)))
-  (other-window -1))
+
+(set-goal-column 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
