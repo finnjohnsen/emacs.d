8e57109fcbee933dbc30cd522fa52c964cfb6450
Finn Johnsen <finn.johnsen@schibsted.com>
2016-09-14 13:44:55 +0200

    start magit automatically when --magit is given

--- init.el
+++ init.el
@@ -51,3 +51,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(if (member "--magit" command-line-args) (magit-status "./"))
