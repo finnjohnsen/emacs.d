86eea5cee54b15febae912f14c9999dca5e55865
Finn Johnsen <johnsenf@gmail.com>
2023-08-04 10:04:20 +0200

    oppdatert theme og zig-mode

--- init.el
+++ init.el
@@ -9,6 +9,8 @@
 (when (not (package-installed-p 'magit)) (package-install 'magit))
 (when (not (package-installed-p 'yaml-mode)) (package-install 'yaml-mode))
 (when (not (package-installed-p 'clojure-mode)) (package-install 'clojure-mode))
+(when (not (package-installed-p 'zig-mode)) (package-install 'zig-mode))
+
 
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
@@ -44,7 +46,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(clojure-mode cljsbuild-mode zenburn-theme yaml-mode magit)))
+   '(zig-mode clojure-mode cljsbuild-mode zenburn-theme yaml-mode magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
