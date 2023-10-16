df80644cce4801e1aab340bcf89c1507c4831769
Finn Johnsen <finn.johnsen@kodemaker.no>
2023-10-16 21:42:26 +0200

    c-/ comment region

--- init.el
+++ init.el
@@ -39,20 +39,14 @@
       (delete-other-windows)))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(zig-mode clojure-mode cljsbuild-mode zenburn-theme yaml-mode magit)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(custom-set-faces)
 
 (xterm-mouse-mode)
+
+; Keybinds
+(global-set-key (kbd "C-c /") 'comment-region)
 
 ;; check OS type
 (cond
