41f1d2faea2d7ae3617f7da8b9ae812cbd17b52e
Finn Johnsen <johnsenf@gmail.com>
2023-11-14 15:00:02 +0100

    emacs gjorde noe automatisk greier

--- init.el
+++ init.el
@@ -40,9 +40,18 @@
       (delete-other-windows)))
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zig-mode clojure-mode cljsbuild-mode zenburn-theme yaml-mode magit)))
-(custom-set-faces)
+   '(xclip zig-mode clojure-mode cljsbuild-mode zenburn-theme yaml-mode magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (xterm-mouse-mode)
 
